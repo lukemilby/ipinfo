1989fa25f821f0c0244839d505c8ed9a132a1f84
bear <bear@bears-MacBook-Pro.local>
2022-09-09 18:21:45 -0500

    on enum used lol, clap reads from env vars! using serde to parse the json to struct

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,53 @@
-use std::env;
 use std::io::Read;
 use clap::{App, Arg};
-// use serde_derive::Deserialize;
-// use serde_derive::Serialize;
+use serde::{Deserialize, Serialize};
+
 
 // read api key from env or config file in ~/.config/ipinfo
-
-// #[derive(Default, Debug, Clone, PartalEq, Serialize, Deserialize)]
-// #[serde(rename_all = "camelCase")]
-// struct IpInfo {
-//     ip: String,
-//     hostname: String,
-//     anycast: bool,
-//     city: String,
-//     region: String,
-//     country: String,
-//     loc: String,
-//     org: String,
-//     postal: String,
-//     timezone: String,
-// }
+#[derive(Deserialize, Serialize)]
+struct IpInfo {
+    ip: String,
+    hostname: String,
+    anycast: bool,
+    city: String,
+    region: String,
+    country: String,
+    loc: String,
+    org: String,
+    postal: String,
+    timezone: String,
+}
 
 
-// error_chain! { // TODO: What is this?
-//     foreign_links {
-//         Io(std::io::Error);
-//         HttpRequest(reqwest::Error);
-//     }
-// }
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let matches = App::new("ipinfo")
         .version("0.1.0")
         .author("Luke Milby")
         .about("Cli tool for ipinfo.io")
-        .arg(Arg::with_name("key")
-            .short("k")
-            .long("key")
-            .takes_value(true)
-            .help("API Key for ipinfo.io"))
         .arg(Arg::with_name("ip")
             .long("ip")
             .takes_value(true)
             .help("IP address to lookup"))
+        .arg(Arg::with_name("key")
+            .short("k")
+            .long("key")
+            .takes_value(true)
+            .env("IPINFO_KEY")
+            .help("API Key for ipinfo.io")
+        )
         .get_matches();
 
     let ip = matches.value_of("ip").unwrap();
-    let api_key = matches.value_of("key").unwrap();
+    let key = matches.value_of("key").expect("missing key");
 
-    let mut res = reqwest::blocking::get(format!("https://ipinfo.io/{}?token={}", ip, api_key))?;
+    let mut res = reqwest::blocking::get(format!("https://ipinfo.io/{}?token={}", ip, key))?;
     let mut body = String::new();
     res.read_to_string(&mut body)?;
+    let payload: IpInfo = serde_json::from_str(body.as_str())?;
+    let s = serde_json::to_string(&payload)?;
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
-
+    println!("{}", s);
     Ok(())
 }
